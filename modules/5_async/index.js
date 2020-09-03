bd236161b4d1c83ea9525224585e4718eaf60c92
Konstantin_Dobrenkii <Konstantin_Dobrenkii@epam.com>
2020-09-03 23:35:10 +0300

    Module 5

--- modules/5_async/index.js
+++ modules/5_async/index.js
@@ -1,17 +1,55 @@
 export const delay = (timeout) => {
+  return new Promise((resolve =>
+      setTimeout(resolve, timeout)));
 };
 
 export const runPromisesInSeries = (promiseCalls) => {
+  promiseCalls.reduce((current, next) => current.then(next), Promise.resolve());
 };
 
 export const Promise_all = (promises) => {
   return new Promise((resolve, reject) => {
-    // Your code here.
+    const results = [];
+    let counter = promises.length;
+    if (counter === 0) {
+      resolve([]);
+    }
+    promises.forEach((promise, index) => {
+      promise.then(value => {
+        results[index] = value;
+        counter--;
+        if (counter === 0) {
+          resolve(results);
+        }
+      })
+      .catch(reason => reject(reason));
+    });
   });
 };
 
-export const fibonacci = (n) => {};
+export const fibonacci = (n) => {
+  return fib(n)
+};
 
-export const helper = () => {};
+function* fib(n, current = 0, next = 1) {
+  if (n === 0) {
+    return current;
+  }
+  yield current;
+  yield* fib(n - 1, next, current + next);
+}
+
+export async function helper (generatorFunction) {
+  const generator = generatorFunction();
+  let generatedValue = await generator.next().value;
+  try {
+    while(generatedValue !== undefined) {
+      generatedValue = await generator.next(generatedValue).value;
+    }
+  } catch(e) {
+    generator.throw(e);
+  }
+};
+
 
 export const MyPromise = () => {};
